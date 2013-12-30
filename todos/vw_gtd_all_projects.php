9c20ca9f2052f538efd8c392eb6525bb9adbdb81
D Keith Casey Jr <keith@caseysoftware.com>
2013-12-29 23:05:10 -0600

    style tweaks to get the right layouts

--- todos/vw_gtd_all_projects.php
+++ todos/vw_gtd_all_projects.php
@@ -8,20 +8,29 @@ $projects = $project->getAllowedProjects($AppUI->user_id, false);
 $todo = new CTodo();
 $todoCategories = w2PgetSysVal('TodoType');
 
-foreach ($projects as $data) {
-    $todos = $todo->loadAll('todo_due_date ASC', 'todo_status = 1 AND todo_owner = ' . $AppUI->user_id . ' AND todo_project = ' . $data['project_id']);
+?>
+<table width="100%">
+    <tr>
+        <td id="todos_list" style="width: 100%; vertical-align: top;">
+            <?php
+            foreach ($projects as $data) {
+                $todos = $todo->loadAll('todo_due_date ASC', 'todo_status = 1 AND todo_owner = ' . $AppUI->user_id . ' AND todo_project = ' . $data['project_id']);
 
-    if (count($todos)) {
-        echo '<strong>'.$data['project_name'].'</strong>';
-        echo '<ul style="list-style-type: none;">';
-        $i = 0;
-        foreach($todos as $todoItem) {
-            $i++;
-            if ($i > 3) {
-                break;
+                if (count($todos)) {
+                    echo '<strong>'.$data['project_name'].'</strong>';
+                    echo '<ul style="list-style-type: none;">';
+                    $i = 0;
+                    foreach($todos as $todoItem) {
+                        $i++;
+                        if ($i > 3) {
+                            break;
+                        }
+                        $todo->renderItem($todoItem, $todoCategories);
+                    }
+                    echo '</ul>';
+                }
             }
-            $todo->renderItem($todoItem, $todoCategories);
-        }
-        echo '</ul>';
-    }
-}
+            ?>
+        </td>
+    </tr>
+</table>
