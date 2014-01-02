7b2fde1b4e35c32897914c8920e6fbd3785ad4ff
D Keith Casey Jr <keith@caseysoftware.com>
2014-01-01 22:13:59 -0600

    tweaked the results ordering; made sure we'd always get the 'no project' todo items;

--- todos/vw_gtd_active_projects.php
+++ todos/vw_gtd_active_projects.php
@@ -3,7 +3,8 @@
 global $AppUI;
 
 $project = new CProject();
-$projects = $project->getAllowedProjects($AppUI->user_id);
+$projects = array(0 => array('project_name' => 'No project specified', 'project_id' => 0 ));
+$projects += $project->getAllowedProjects($AppUI->user_id);
 
 $todo = new CTodo();
 $todoCategories = w2PgetSysVal('TodoType');
@@ -14,7 +15,7 @@ $todoCategories = w2PgetSysVal('TodoType');
         <td id="todos_list" style="width: 100%; vertical-align: top;">
             <?php
             foreach ($projects as $data) {
-                $todos = $todo->loadAll('todo_due_date ASC', 'todo_status = 1 AND todo_owner = ' . $AppUI->user_id . ' AND todo_project = ' . $data['project_id']);
+                $todos = $todo->loadAll('todo_due_date', 'todo_status = 1 AND todo_owner = ' . $AppUI->user_id . ' AND todo_project = ' . $data['project_id']);
 
                 if (count($todos)) {
                     echo '<strong>'.$data['project_name'].'</strong>';
